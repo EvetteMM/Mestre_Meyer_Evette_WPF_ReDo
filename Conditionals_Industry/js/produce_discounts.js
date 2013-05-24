c2e1f2b3393ba450cc3bfbd1675d84ebeda2cf66
Evette Mestre-Meyer <evette@emmmarketing.com>
2013-05-23 21:32:59 -0400

    Conditionals - Industry Produce Discounts - can't figure out why I can't view in Firefox - nothing is coming up

---
+++ Conditionals_Industry/js/produce_discounts.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<script type="text/javascript">
+
+//Evette Mestre Meyer_5-23-13_Conditionals_Industry - Produce Discounts
+//Greenacre Farms is offering a 10% Discount on orders over 50 pounds OR over $250
+//Ppp is shorthands for price per pound
+
+var lbsLettuce = Number(prompt("How many pounds of lettuce would you like today?"), "");
+
+var lettucePpp;
+lettucePpp = .50;
+
+var lettuceTotal;
+lettuceTotal = (lbsLettuce * lettucePpp);
+
+var lbsTomatoes = Number(prompt("How many pounds of tomatoes would you like today?"), "");
+var tomatoesPpp;
+tomatoesPpp = .50;
+
+var tomatoesTotal;
+tomatoesTotal = (lbsTomatoes * tomatoesPpp);
+
+var totalPoundsOrdered;
+totalPoundsOrdered = lbsLettuce + lbsTomatoes;
+
+var orderTotal;
+orderTotal = lettuceTotal + tomatoesTotal;
+
+if ((totalPoundsOrdered >= 50) || (orderTotal >= 250))
+{
+	document.write("You receive a 10% Discount today!  Thank you for your order.");
+}
+
+</script>
+</body>
+</html>
