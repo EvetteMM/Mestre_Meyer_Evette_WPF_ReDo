728d550e67524732ce5bccaba8c99648a0726ebf
EvetteMM <evette@emmmarketing.com>
2013-05-29 15:11:49 -0400

    Functions_Personal added notes for rationale

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -5,9 +5,10 @@
 <script type="text/javascript">
 //Evette Mestre Meyer_5-30-13_Functions_Personal
 //School is offering 15% discount on tuition for next year if paid by June 1st.  Temple is offering 20% discount on membership if paid by June 1st.  If I have to pick one, which will save me more money?
+//Get tuition and membership costs first
 var schoolTuition =Number(prompt("How much is school tuition next year?", ""));
 var templeMembership =Number(prompt("How much is temple membership next year?", ""));
-
+//calculate discounts for each to get total savings
 function calcBetterSavings(schoolTuition, templeMembership){
 	var schoolTuition;
 	var templeMembership;
@@ -15,10 +16,11 @@ function calcBetterSavings(schoolTuition, templeMembership){
 	var templeDiscount = .20;
 	var tuitionSavings = schoolTuition * schoolDiscount;
 	var membershipSavings = templeMembership * templeDiscount;
+	//compare savings for each
 	console.log("The tuition savings for next year is $" + tuitionSavings ". The total temple membership for next year is $" + membershipSavings + ".");
 }
 calcBetterSavings(schoolTuition, templeMembership);
-
+//make decision about which to pay
 if (tuitionSavings >= membershipSavings)
 {
 	document.write("Pay the school by June 1st.");
