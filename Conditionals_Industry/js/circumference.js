ba4ebaea3addd279b85f609f57e4c10dbaa56ea7
EvetteMM <evette@emmmarketing.com>
2013-05-29 11:11:26 -0400

    Functions_Worksheet_Circumference tried testing area function example and it's still not working in Firefox - can't figure out what I am doing wrong

---
+++ Conditionals_Industry/js/circumference.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<script type="text/javascript">
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+</script>
+</body>
+</html>
