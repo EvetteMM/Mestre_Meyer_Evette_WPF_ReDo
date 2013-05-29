f4aefba22a8f7754323052f35bdb42d491ca214a
EvetteMM <evette@emmmarketing.com>
2013-05-29 13:18:46 -0400

    Functions_Personal Just set up link between index and js no code yet

---
+++ Functions_Personal/js/personal.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<script type="text/javascript">
+//Evette Mestre Meyer_5-30-13_Functions_Personal
+
+</script>
+</body>
+</html>
