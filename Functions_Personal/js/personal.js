86fdc9b04848d30d7bd702e019d3140cdc37df4c
EvetteMM <evette@emmmarketing.com>
2013-05-29 15:09:37 -0400

    Functions_Personal Set up code with prompt Tried adding conditionals Not working in Firefox

--- Functions_Personal/js/personal.js
+++ Functions_Personal/js/personal.js
@@ -4,6 +4,29 @@
 <body>
 <script type="text/javascript">
 //Evette Mestre Meyer_5-30-13_Functions_Personal
+//School is offering 15% discount on tuition for next year if paid by June 1st.  Temple is offering 20% discount on membership if paid by June 1st.  If I have to pick one, which will save me more money?
+var schoolTuition =Number(prompt("How much is school tuition next year?", ""));
+var templeMembership =Number(prompt("How much is temple membership next year?", ""));
+
+function calcBetterSavings(schoolTuition, templeMembership){
+	var schoolTuition;
+	var templeMembership;
+	var schoolDiscount = .15;
+	var templeDiscount = .20;
+	var tuitionSavings = schoolTuition * schoolDiscount;
+	var membershipSavings = templeMembership * templeDiscount;
+	console.log("The tuition savings for next year is $" + tuitionSavings ". The total temple membership for next year is $" + membershipSavings + ".");
+}
+calcBetterSavings(schoolTuition, templeMembership);
+
+if (tuitionSavings >= membershipSavings)
+{
+	document.write("Pay the school by June 1st.");
+}
+else if (tuitionSavings <= membershipSavings)
+{
+	document.write("Pay the temple by June 1st");
+}
 
 </script>
 </body>
