aa5d8d695d60acbaef3fee0ac6025142e329be69
Evette Mestre-Meyer <evette@emmmarketing.com>
2013-05-29 18:09:39 -0400

    Take a look

--- Functions_Industry/js/industry.js
+++ Functions_Industry/js/industry.js
@@ -1,8 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<body>
-<script type="text/javascript">
 //Evette Mestre Meyer_5-30-13_Functions_Industry
 //Calculate gross pay including overtime
 var hrsWorked =Number(prompt("How many hours did you work this week?", ""));
@@ -18,6 +13,3 @@ function calcGrossPay(){
 }
 calcGrossPay(hrsWorked);
 
-</script>
-</body>
-</html>
