6b548821bf12eea8f53c0ca5931ec8612157061f
EvetteMM <evette@emmmarketing.com>
2013-05-29 11:10:37 -0400

    Functions_Worksheet_Stung - just like Circumference - it's not working in Firefox - have double checked everything I can think of and can't get it to work

---
+++ Functions_worksheet/stung/js/stung.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<script type="text/javascript">
+//Evette Mestre Meyer_5-30-13_Functions_Worksheet_Stung
+//It takes 8.666666667 bee stings PER POUND to kill an animal
+//Calculate how many bee stings to kill an animal of a certain weight
+function calcKill(){
+	var beeStingsPerPound = 8.666666667;
+	var animalWeight = 10;
+	var kill = beeStingsPerPound * animalWeight;
+	console.log=("It takes " + beeStingsPerPound + "to kill a " + animalWeight + " pound dog.");
+}
+calKill();
+</script>
+</body>
+</html>
