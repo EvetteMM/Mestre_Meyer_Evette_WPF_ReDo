37038b706994bbb582c2efbc779cc0f79d302637
EvetteMM <evette@emmmarketing.com>
2013-05-29 13:17:58 -0400

    Functions_Industry GrossPay - set up calculations to calculate gross pay (including reg pay and overtime)

---
+++ Functions_Industry/js/functions.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<script type="text/javascript">
+//Evette Mestre Meyer_5-30-13_Functions_Industry
+//Calculate gross pay including overtime
+var hrsWorked =Number(prompt("How many hours did you work this week?", ""));
+
+function calcGrossPay(){
+	var hoursWorked;
+	var regHours = 30;
+	var otHours = (hoursWorked - regHours);
+	var regRate = 10;
+	var otRate = 15;
+	var grossPay = ((regHours * regRate) + (otHours * otRate));
+}
+calcGrossPay(hrsWorked);
+
+</script>
+</body>
+</html>
