49f37bbaee6c69fbbff575c3b49ed7d9edb38144
EvetteMM <evette@emmmarketing.com>
2013-05-29 11:09:13 -0400

    Functions_Worksheet_Circumference Can't get it to work in Firefox Keep getting this message [11:04:03.409] file:///Users/EvetteLaptop/Dropbox/FSU/WPF%20Repository/Functions_worksheet/stung/js/stung.js

--- Functions_worksheet/circumference/js/circumference.js
+++ Functions_worksheet/circumference/js/circumference.js
@@ -2,20 +2,18 @@
 "http://www.w3.org/TR/xhtmll/DTD/xhtmll-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <body>
+
 <script type="text/javascript">
-//Evette Mestre Meyer_5-30-13_Functions Worksheet_Circumference
-//Calculate the circumference of a circle
-var radius = (prompt("What is the radius of your circle?", ""));
-var piTwo = 3.14 * 2;
- 
-function calcCircum(radius, piTwo){
-	var circum = radius * piTwo;
-	return circum;
+//Evette Mestre Meyer_5-30-13_Functions_Worksheet_Circumference
+//calculate the circumferene of a circle
+function calcCircumference(){
+	var pi = 3.14;
+	var radius = 10;
+	var doubleRadius = 2;
+	var circumference = (pi * radius * doubleRadius);
+	document.write(circumference);
 }
-
-var totalCircum = calcCircum(radius, piTwo);
-console.log(totalCircum);
-
+calcCircumference();
 </script>
 </body>
 </html>
