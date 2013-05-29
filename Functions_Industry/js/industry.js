d42efa048517510ac0bc3b39f4ff98860acb6ad4
Evette Mestre-Meyer <evette@emmmarketing.com>
2013-05-29 18:24:54 -0400

    changed calcGrossPay(hoursWorked)

--- Functions_Industry/js/industry.js
+++ Functions_Industry/js/industry.js
@@ -1,8 +1,8 @@
 //Evette Mestre Meyer_5-30-13_Functions_Industry
 //Calculate gross pay including overtime
-var hrsWorked =Number(prompt("How many hours did you work this week?", ""));
+var hoursWorked =Number(prompt("How many hours did you work this week?", ""));
 //function needs to calculate how many overtime hours first.  Then multiply reg hourly rate by reg hours and ot hourly rate by ot hours
-function calcGrossPay(hrsWorked){
+function calcGrossPay(hoursWorked){
 	var regHours = 30;
 	var otHours = (hoursWorked - regHours);
 	var regRate = 10;
@@ -10,5 +10,5 @@ function calcGrossPay(hrsWorked){
 	var grossPay = ((regHours * regRate) + (otHours * otRate));
 	console.log("Your regular salary and overtime pay total $" + grossPay);
 }
-calcGrossPay(hrsWorked);
+calcGrossPay(hoursWorked);
 
