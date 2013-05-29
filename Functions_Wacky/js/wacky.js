50914b940eefc8463d30200bb138d309f664ad27
EvetteMM <evette@emmmarketing.com>
2013-05-29 14:08:22 -0400

    Functions_Wacky set up code tried adding prompts, conditionals not working in Firefox

--- Functions_Wacky/js/wacky.js
+++ Functions_Wacky/js/wacky.js
@@ -4,6 +4,30 @@
 <body>
 <script type="text/javascript">
 //Evette Mestre Meyer_5-30-13_Functions_Wacky
+//Can Amanda get a new iPod?
+//Amanda has money from chores and birthday gifts saved up.  An iPod costs $199. Does she have enough yet?
+var choreMoney =Number(prompt("How much chore money have you saved up?", ""));
+var bdayMoney =Number(prompt("How much birthday money have you saved up?", ""));
+//Figure out if money saved is enough to buy an iPod
+function calcEnoughSaved(choreMoney, bdayMoney){
+	var choreMoney;
+	var bdayMoney;
+	var totalMoneySaved = choreMoney + bdayMoney;
+	var iPodCost = 199;
+	var enoughSaved = totalMoneySaved - iPodCost;
+	console.log("Amanda has $" + totalMoneySaved + " saved.")
+}
+calcEnoughSaved(choreMoney, bdayMoney);
+//If money saved is greater than cost of iPod, Amanda has enough
+if (enoughSaved >= iPodCost)
+{
+	document.write("Great job, Amanda - you can buy a new iPod!");
+}
+//If money saved is less than cost of iPod, she needs to keep saving
+else if (enoughSaved <= iPodCost)
+{
+	document.write("Keep saving, Amanda.  You'll get a new iPod soon.");
+}
 
 </script>
 </body>
