ab6dcb3f6261de618a57a47b0da9ecca86d3276f
Evette Mestre-Meyer <evette@emmmarketing.com>
2013-05-29 22:15:33 -0400

    Functions Worksheet_Stung added notes created variable for result

--- Functions_worksheet/stung/js/stung.js
+++ Functions_worksheet/stung/js/stung.js
@@ -1,12 +1,15 @@
 //Evette Mestre Meyer_5-30-13_Functions_Worksheet_Stung
 //It takes 8.666666667 bee stings PER POUND to kill an animal
 //Calculate how many bee stings to kill an animal of a certain weight
-var animalWeight =(prompt("What is the weight of the animal?"));
+var animalWeight =(prompt("What is the weight of the dog?"));
 //Calculate variable into function
 function calcKill(animalWeight){
+//write the givens
 	var beeStingsPerPound = 8.666666667;
 	var kill = beeStingsPerPound * animalWeight;
+	return kill;
 	console.log=("It takes " + beeStingsPerPound + "to kill a " + animalWeight + " pound dog.");
 }
 //Invoke function
-calKill(animalWeight);
+calcKill(animalWeight);
+var calcKill;
