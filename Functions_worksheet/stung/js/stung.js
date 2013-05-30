19b89639dc8ae2270fd4495e5c054be927b49a8e
Evette Mestre-Meyer <evette@emmmarketing.com>
2013-05-29 22:03:16 -0400

    Functions_Worksheet added prompt for animal weight added animal weight to parameters tried running in Firefox - no luck

--- Functions_worksheet/stung/js/stung.js
+++ Functions_worksheet/stung/js/stung.js
@@ -1,10 +1,12 @@
 //Evette Mestre Meyer_5-30-13_Functions_Worksheet_Stung
 //It takes 8.666666667 bee stings PER POUND to kill an animal
 //Calculate how many bee stings to kill an animal of a certain weight
-function calcKill(){
+var animalWeight =(prompt("What is the weight of the animal?"));
+//Calculate variable into function
+function calcKill(animalWeight){
 	var beeStingsPerPound = 8.666666667;
 	var animalWeight = 10;
 	var kill = beeStingsPerPound * animalWeight;
 	console.log=("It takes " + beeStingsPerPound + "to kill a " + animalWeight + " pound dog.");
 }
-calKill();
+calKill(animalWeight);
